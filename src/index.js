e027114edf6e4178a3bd1095480ea35b6dbec458
Kamil Sz <kamil.szewczyk9304@gmail.com>
2022-06-03 18:55:14 +0200

    Own State

--- src/index.js
+++ src/index.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { useCallback,  } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
 
+let state = null;
+
+function useState(defaultValue) {
+  const setValue = (newValue) => { };
+
+  const currentState = [defaultValue, setValue];
+
+  return [defaultValue, setValue];
+}
+
+function TestHook() {
+
+   const [value, setValue] = useState('start')
+
+  return (
+    <>
+      <h2>Test Hook!</h2>
+      <input
+        tyte='text'
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      ></input>
+    </>
+  )
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <TestHook/>
+   {/* <App />*/}
   </React.StrictMode>
 );
 
