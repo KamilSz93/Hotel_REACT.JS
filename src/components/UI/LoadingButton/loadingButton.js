c9ed6e399430d2421eb9a66050ec45cf18370e0c
Kamil Sz <kamil.szewczyk9304@gmail.com>
2022-06-23 18:43:49 +0200

    form valid

---
+++ src/components/UI/LoadingButton/loadingButton.js
@@ -0,0 +1,14 @@
+export default function LoadingButton (props) {
+
+    const className = props.className || 'btn-primary';
+
+    return (
+        props.log
+            ? (
+                <button className={`btn ${className} mt-3 `} type="button" disabled>
+                  <span className="spinner-border spinner-border-sm"role="status"aria-hidden="true"></span>
+                  <span className="sr-only">Loading...</span>
+                </button>
+            ) : <button className={`btn ${className} mt-2`}>{ props.children }</button>
+    );
+}
--- src/pages/Auth/Login/login.js
+++ src/pages/Auth/Login/login.js
@@ -1,41 +1,65 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
+import useAuth from '../../../hooks/useAuth';
+import { useHistory } from 'react-router-dom';
+import LoadingButton from "../../../components/UI/LoadingButton/loadingButton";
 
 export default function Login(props) {
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
 
-
+  const [auth, setAuth] = useAuth();
+  const history = useHistory();
+  const [valid, setValid] = useState(null);
 
   const submit = (e) => {
     e.preventDefault();
+    setLoading(true)
     
     setTimeout(()=> {
-      //logowanie
+      if (true) {
+        setAuth(true);
+        history.push('/');
+      } else {
+        setValid(false);
+        setPassword("");
+      }
+        setLoading(false);
     }, 500)
-
   };
 
   return (
     <div>
+      <h2>Logowanie</h2>
+      {valid === false ? (
+        <div className="alert alert-danger">Niepoprawne dane logowania</div>
+      ) : (null)
+      }
       <form onSubmit={submit}>
         <div className="form-group">
           <label>Email</label>
           <input
             value={email}
-            onChange={(e)=>{setEmail(e.target.value)}}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
             type="email"
-            className="form-control" />
+            className="form-control"
+          />
         </div>
         <div className="form-group">
           <label>Hasło</label>
           <input
             value={password}
-            onChange={(e)=>{setPassword(e.target.value)}}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
             type="password"
-            className="form-control" />
+            className="form-control"
+          />
         </div>
-        <button className="btn btn-primary mt-2">Zapisz</button>
+        <LoadingButton log={ loading } > Zaloguj </LoadingButton>
       </form>
     </div>
   );
